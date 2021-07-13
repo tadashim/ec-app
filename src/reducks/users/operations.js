9f3a7870ad7ad765f9f0b7993299e4fd236f39d8
tadashim <ma.tadashi@gmail.com>
2021-07-13 23:34:40 +0900

    Add Sign up and Sign in pages

--- src/reducks/users/operations.js
+++ src/reducks/users/operations.js
@@ -1,25 +1,76 @@
 import {signInAction} from './actions';
 import {push} from "connected-react-router";
+import {auth, db, FirebaseTimestamp} from "../../firebase/index"
 
-export const signIn = () => {
-  return async (dispatch, getState) => {
-    const state = getState()
-    const isSignedIn = state.users.isSignedIn
-
-    if (!isSignedIn) {
-      const url = 'https://api.github.com/users/tadashim'
-
-      const response = await fetch(url)
-                          .then(res => res.json())
-                          .catch(() => null)
-      const username = response.login
-
-      dispatch(signInAction({
-        isSignedIn: true,
-        uid: "0001",
-        username: username
-      }))
-      dispatch(push('/'))
+export const signIn = (email, password) => {
+  return async (dispatch) => {
+    // Validation
+    if (email === "" || password === "") {
+      alert("必須項目が未入力です。");
+      return false;
     }
+
+    auth.signInWithEmailAndPassword(email, password)
+      .then(result => {
+        const user = result.user;
+
+        if (user) {
+          const uid = user.uid;
+
+          db.collection('users').doc(uid).get()
+            .then(snapshot => {
+              const data = snapshot.data()
+
+              dispatch(signInAction({
+                isSignedIn: true,
+                role: data.role,
+                uid: uid,
+                username: data.username
+              }))
+
+              dispatch(push('/'))
+
+            })
+        }
+      })
+  }
+}
+
+export const signUp = (username, email, password, confirmPasswowrd) => {
+  return async (dispatch) => {
+    // Validation
+    if (username === "" || email === "" || password === "" || confirmPasswowrd === "") {
+      alert("必須項目が未入力です。");
+      return false;
+    }
+
+    if (password !== confirmPasswowrd) {
+      alert("パスワードが一致しません。もう一度お試しください。")
+      return false;
+    }
+
+    return auth.createUserWithEmailAndPassword(email, password)
+        .then(result => {
+          const user = result.user;
+
+          if (user) {
+            const uid = user.uid;
+            const timestamp = FirebaseTimestamp.now()
+
+            const userInitialData = {
+              created_at: timestamp,
+              email: email,
+              role: "customer",
+              uid: uid,
+              updated_at: timestamp,
+              username: username
+            }
+
+            db.collection('users').doc(uid).set(userInitialData)
+              .then(() => {
+                dispatch(push('/'))
+              })
+          }
+        });
   }
 }
